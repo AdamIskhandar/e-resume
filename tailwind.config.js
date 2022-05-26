e953bbfc51dd776e297ca1f5084b008015b57e75
AdamIskhandar <88932994+AdamIskhandar@users.noreply.github.com>
2022-05-26 15:12:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'backgorund': "url('/img/bg.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
